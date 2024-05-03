6dd22e443678db0d7d069a760cb7bbc8ab2df917
Michel Abele <michel@abele.onl>
2024-05-03 18:38:06 +0200

    cookie info
    cookie info

--- dist/html/file-foot.php
+++ dist/html/file-foot.php
@@ -2,7 +2,14 @@
 ob_start();
 ?>
 
-<!-- JavaScript -->
+    <div id="cookie-notice" class="alert alert-info">
+        <div class="container">
+            <p>Ich und Drittanbieter die ich nutze verwenden Cookies, um Dir das beste Erlebnis auf meiner Webseite zu bieten. Durch die Nutzung meiner Webseite stimmst Du der Verwendung von Cookies zu.</p>
+            <button id="cookie-accept" class="btn btn-primary">Verstanden</button>
+        </div>
+    </div>
+
+    <!-- JavaScript -->
     <script src="/lib/js/jquery.js"></script>
     <script src="/lib/js/bootstrap.bundle.js"></script>
     <script src="/lib/js/custom.js"></script>
