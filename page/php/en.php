0ed58c6fc03fffaf781d02e38d0a0ac285e83425
Michel Abele <michel@abele.onl>
2024-05-10 19:31:22 +0200

    ext 2024-05-10 19:31
    ext 2024-05-10 19:31

--- page/php/en.php
+++ page/php/en.php
@@ -1,15 +1,50 @@
 <?php
 
 # page settings
-$language = 'en';
-$title = 'Digital portfolio';
-$description = 'Digital portfolio of Michel Abele, IT specialist and forwarding agent from Hohenwarsleben (Germany).';
+$var_page = [
+    'language'    => 'en',
+    'title'       => 'Digital portfolio',
+    'description' => 'Digital portfolio of Michel Abele, IT specialist and forwarding agent from Hohenwarsleben (Germany).'
+];
 
-ob_start();
-?>
+# navbar
+$var_navbar = [
+    'start' => 'Home',
+    'about' => 'About Me',
+    'certificates' => 'Certificates',
+    'skills' => 'Skills',
+    'resume' => 'Resume',
+    'github' => 'GitHub Repositories',
+    'websites' => 'Websites',
+    'games' => 'Game Servers',
+    'contact' => 'Contact'
+];
 
+# typed
+$var_typed = [
+    'Windows Administrator',
+    'Linux Administrator',
+    'Network Administrator',
+    'Full Stack Web Developer',
+    'Freight Forwarding Agent',
+    'Truck Dispatcher',
+    'Truck Driver',
+    'Local Historian'
+];
+$var_page['typed'] = implode(',', $var_typed);
 
+# age
+$birthDate = "1980-02-03";
+$currentDate = date("Y-m-d");
+$var_page['age'] = date_diff(date_create($birthDate), date_create($currentDate))->y;
 
-<?php
-$content = ob_get_contents();
-ob_end_clean();
+# content
+$content  = file_get_contents('../html/en/about.html');
+$content .= file_get_contents('../html/en/facts.html');
+$content .= file_get_contents('../html/en/certificates.html');
+$content .= file_get_contents('../html/en/skills.html');
+$content .= file_get_contents('../html/en/resume.html');
+$content .= file_get_contents('../html/en/github.html');
+$content .= file_get_contents('../html/en/websites.html');
+$content .= file_get_contents('../html/en/games.html');
+$content .= file_get_contents('../html/en/contact.html');
